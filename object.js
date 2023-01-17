9ed0e16747f2daa3f55164d78904e1765a51353e
Akhileshkumar001 <aryansingh724112@gmail.com>
2023-01-18 00:42:43 +0530

    comitted

---
+++ object.js
@@ -0,0 +1,17 @@
+let captainAmerica={
+    firstname: "steve",
+    lasrName: "rogers",
+    friends:["buckey" ,"tony" , "brue banner"],
+    age : 104 ,
+    isAvenger:"true",
+    Adress:{
+        country : "America",
+        city : "khi v",
+    }
+
+}
+console.log(captainAmerica);
+for(let key in captainAmerica){
+    console.log(key);
+
+}
