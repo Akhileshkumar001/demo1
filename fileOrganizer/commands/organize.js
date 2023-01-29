b717b71ba418ebec594d57e11865df5040bd579a
Akhileshkumar001 <aryansingh724112@gmail.com>
2023-01-29 20:00:38 +0530

    commited

--- fileOrganizer/commands/organize.js
+++ fileOrganizer/commands/organize.js
@@ -25,12 +25,30 @@ function organize(srcPath){
     }
     // 3. all file entire srcPath(downloads folder in the case)
     let allFile=fs.readdirSync(srcPath);//Reads the contents of the directory->basically reads the name of files present in the directory
-    console.log(allFile);
+    //console.log(allFile);
     for(let i=0;i<allFile.length;i++){
-        let ext=allFile[i].split(".")[1];
+       // let ext=allFile[i].split(".")[1];
         // or
         //let ext=path.extname(allFiles[i])
-        console.log(ext);
+       // console.log(ext);
+       // extname returns the extension of the file
+       let fullPathFile = path.join(srcPath,allFile[i]);
+      // console.log(fullPathFile);
+      //1. check if it is a file or not 
+      // lstatSync gives the information regarding this link provided
+      let isFile=fs.lstatSync(fullPathFile).isFile(); // true-> file hai to or false->file nhi hai to
+      console.log(allFile[i]+" is "+isFile);
+      if(isFile){
+        // 1.1 get ext name
+        let ext=path.extname(allFile[i]).split(".")[1];
+       // console.log(ext);
+       //1.2 get folder name from extension
+       let folderName=getfolderNmae(ext);// archives
+       // 1.3 copy from src folder (srcPath) and paste in dest folder (folder_name e.g. document,media etc)
+       copyFileToDest(srcPath,fullPathFile,folderName)
+    
+        }
+
     }
 }
 let srcPath="/Users/91834/Desktop/gitDemo/fileOrganizer/downloads"
