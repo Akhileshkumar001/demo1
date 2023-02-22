76beac53e77c2283e9abb145fee1e96432bf4c6b
Akhileshkumar001 <aryansingh724112@gmail.com>
2023-02-23 00:51:53 +0530

    commited

--- Browser/jira/script.js
+++ Browser/jira/script.js
@@ -1,10 +1,16 @@
+var uid = new ShortUniqueId();
 const modlCont=document.querySelector(".model-cont")
 const addbtn=document.querySelector(".add-btn")
 const allPriorityColor=document.querySelectorAll(".priority-color");
 let colors = ["lightpink" , "lightgreen" , "lightblue" , "black"];
 let modalapriorityColor=colors[colors.length-1];
 let textArea=document.querySelector(".textarea-cont");
+let mainCont=document.querySelector(".main-containt");
+let ticketArr=[];
+let toolticketColor=document.querySelectorAll(".color");
+let removebtn=document.querySelector(".remove-btn");
 
+//to open close modal container
 let isModalPresent= false;
 
 addbtn.addEventListener('click',function(){
@@ -15,6 +21,8 @@ addbtn.addEventListener('click',function(){
     }
     isModalPresent=!isModalPresent; //toggling effect
 });
+
+// to remove  and add active class from each priority color of modal container
   allPriorityColor.forEach(function(elmentColor){
     elmentColor.addEventListener("click",function(){
         allPriorityColor.forEach(function(priorityColorElm){
@@ -27,13 +35,14 @@ addbtn.addEventListener('click',function(){
     });    
 });
 
+// to generate and display a ticket
 modlCont.addEventListener("keydown",function (e) {
     let key = e.key;
     if(key == "Shift"){
         console.log(modalapriorityColor);
         console.log(textArea.value);
         createTicket(modalapriorityColor,textArea.value);
-        modlCont.style.display="none";
+        modlCont.style.display = "none";
         isModalPresent = false;
         textArea.value ="";
         allPriorityColor.forEach(function(elmentColor){
@@ -42,11 +51,90 @@ modlCont.addEventListener("keydown",function (e) {
     }
 });
 
+//function to create new ticket 
 function createTicket(ticketColor , data ,ticketId){
-    let ticketCont=document.createAttribute("div");   //just like <div></div>
-    ticketCont.setAttribute("class","ticket-cont");
+    let id = ticketId || uid();
+    let ticketCont=document.createElement("div");   //just like <div></div>
+    ticketCont.setAttribute("class" , "ticket-cont");
     ticketCont.innerHTML=`
-        <div class="ticket-color ${ticketColor}"></div>
-        <div class="ticket-id ${id}"></div>
-        <div class="ticket-text ${data}"></div>`
+    <div class="ticket-color ${ticketColor}"></div>     
+    <div class="ticket-id"> ${id}</div>
+    <div class="ticket-text">${data}</div>
+        `;
+        mainCont.appendChild(ticketCont);
+
+
+    if(!ticketId){
+        ticketArr.push(
+           {
+                ticketColor,
+                data,
+                ticketId:id
+            }
+        );
+        localStorage.setItem("tickets",JSON.stringify(ticketArr)); //,JSON.stringify(ticketArr) it is use for convert object in to string
+    }
+} 
+// get all tickets from local storage   
+if(localStorage.getItem("tickets")){
+    ticketArr=JSON.parse(localStorage.getItem("tickets"));//JSON.parse(localStorage.getItem("tickets")); IT IS USE FOR convert string into object
+    ticketArr.forEach(function(ticketObj){
+        createTicket(ticketObj.ticketColor,ticketObj.data,ticketObj.ticketId);
+    })
 }
+//console.log(uid());/
+// filter on the besis of ticket color
+for(let i=0;i<toolticketColor.length;i++){
+    toolticketColor[i].addEventListener("click",function(){
+       let curentcolortool=toolticketColor[i].classList[0];
+
+       let filterTicket=ticketArr.filter(function(ticketObj){
+        return curentcolortool == ticketObj.ticketColor;
+        //if(curentcolortool == ticketObj.ticketColor);
+        //return ticketObj ;
+       });
+       // remove all the tickets
+       let allticket=document.querySelectorAll(".ticket-cont");
+       for(let i=0;i<allticket.length;i++){
+            allticket[i].remove();
+       }
+       // displa all the tickets
+       filterTicket.forEach(function (ticketObj){
+        createTicket(
+            ticketObj.ticketColor,
+            ticketObj.data,
+            ticketObj.ticketId
+            );
+       })
+       
+    })
+    // to display all the tickets of all colors on duble clicking
+    toolticketColor[i].addEventListener("dblclick",function(){
+        // remove all the  color specific tickets
+     allticket=document.querySelectorAll(".ticket-cont");
+       for(let i=0;i<allticket.length;i++){
+            allticket[i].remove();
+       }
+       // dispalay all ticket
+       ticketArr.forEach(function(ticketObj){
+        createTicket(
+            ticketObj.ticketColor,
+            ticketObj.data,
+            ticketObj.ticketId
+            );
+       });
+
+    })
+}
+let removeBtnActive=false;
+removebtn.addEventListener("click",function(){
+    if(removeBtnActive){
+        removebtn.style.color = "white";
+    }else{
+        removebtn.style.color = "red";
+    }
+    removeBtnActive=!removeBtnActive;
+})
+
+
+
